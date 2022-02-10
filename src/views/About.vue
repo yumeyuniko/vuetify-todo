85d6f410e4924841991f31edb4b5344d72ee6b93
yumeyuniko <daigocafe@msn.com>
2022-02-10 19:58:34 +0900

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium vitae
+      ullam dolore, eveniet reiciendis natus praesentium rerum officia molestiae
+      ipsum animi, dicta aliquid temporibus perspiciatis iusto nobis deserunt
+      sit delectus!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium vitae
+      ullam dolore, eveniet reiciendis natus praesentium rerum officia molestiae
+      ipsum animi, dicta aliquid temporibus perspiciatis iusto nobis deserunt
+      sit delectus!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium vitae
+      ullam dolore, eveniet reiciendis natus praesentium rerum officia molestiae
+      ipsum animi, dicta aliquid temporibus perspiciatis iusto nobis deserunt
+      sit delectus!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium vitae
+      ullam dolore, eveniet reiciendis natus praesentium rerum officia molestiae
+      ipsum animi, dicta aliquid temporibus perspiciatis iusto nobis deserunt
+      sit delectus!
+    </p>
+  </div>
+</template>
